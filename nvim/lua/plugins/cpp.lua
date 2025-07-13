c487f84b2fb3039ca3a18f6c4c63eb9c24651371
Said Alghabra <alghabra.said@gmail.com>
2025-07-12 20:54:45 -0500

    add cpp plugins

---
+++ nvim/lua/plugins/cpp.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      if type(opts.servers) ~= "table" then
+        return
+      end
+
+      local clangd = opts.servers.clangd or {}
+      clangd.cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=never",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+        "--inlay-hints=false",
+      }
+
+      if type(clangd.keys) == "table" then
+        vim.list_extend(clangd.keys, {
+          { "gt", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        })
+      end
+
+      opts.servers.clangd = clangd
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["cpp"] = { "clang-format" },
+        ["c"] = { "clang-format" },
+      },
+    },
+  },
+}
