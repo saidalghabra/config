49ddb6c0d5d2e3b895d5a2942625145705291296
Said Alghabra <alghabra.said@gmail.com>
2025-07-12 21:56:59 -0500

    add codecompanion

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "OXY2DEV/markview.nvim",
+    },
+    opts = {
+      ensure_installed = {
+        "markdown",
+        "markdown_inline",
+      },
+    },
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    "echasnovski/mini.diff",
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "OXY2DEV/markview.nvim",
+    },
+  },
+}
