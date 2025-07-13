c8a38e08b3198660963e97304d249690b693c67e
Said Alghabra <said.alghabra@subzero.com>
2025-07-12 21:51:15 -0500

    add copilot plugins

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,71 @@
+return {
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = { enabled = false }, -- disable suggestion if using blink.cmp
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+
+  -- add ai_accept action
+  {
+    "zbirenbaum/copilot.lua",
+    opts = function()
+      LazyVim.cmp.actions.ai_accept = function()
+        if require("copilot.suggestion").is_visible() then
+          LazyVim.create_undo()
+          require("copilot.suggestion").accept()
+          return true
+        end
+      end
+    end,
+  },
+
+  -- lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(
+        opts.sections.lualine_x,
+        2,
+        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    -- dependencies = { "giuxtaposition/blink-cmp-copilot" },
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            kind = "Copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+}
